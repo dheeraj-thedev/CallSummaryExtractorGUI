a2fe8640a367452b98e4ffdeb829812d9ec7085a
OpenSource <OpenSource@TrainingBasket>
2018-07-09 10:29:14 +0530

    Login Update

--- src/com/trainingbasket/callsummaryextractor/ui/TextToCSVConverter.java
+++ src/com/trainingbasket/callsummaryextractor/ui/TextToCSVConverter.java
@@ -324,12 +324,12 @@ public class TextToCSVConverter extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+/*    public static void main(String args[]) {
+         Set the Nimbus look and feel 
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+         
         try {
             javax.swing.UIManager.LookAndFeelInfo[] installedLookAndFeels = javax.swing.UIManager.getInstalledLookAndFeels();
             for (int idx = 0; idx < installedLookAndFeels.length; idx++) {
@@ -350,14 +350,14 @@ public class TextToCSVConverter extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
 
-        /* Create and display the form */
+         Create and display the form 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TextToCSVConverter().setVisible(true);
             }
         });
     }
-
+*/
     // Variables declaration - do not modify                     
     private javax.swing.JButton btnAddEmail;
     private javax.swing.JButton btnBrowseFile;
